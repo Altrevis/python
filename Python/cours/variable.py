b4e9581585b78dd4a97c7390f115c835b4b8bc25
beleo <leo.benazeth@ynov.com>
2024-02-23 10:25:24 +0100

    cours

---
+++ Python/cours/variable.py
@@ -0,0 +1,4 @@
+x = 5
+y = "Hello, World!"
+print(x)
+print(y)
---
+++ Python/cours/variable2.py
@@ -0,0 +1,12 @@
+x = str("hello")
+y = int(5)
+z = float(3)
+"""
+attention! : si on veux mettre un print(int(x)) 
+qui va donc chercher
+la valeur de x qui est un str
+alors vous aurez un message d'erreur
+"""
+print(str(x))
+print(int(y))
+print(float(z))
